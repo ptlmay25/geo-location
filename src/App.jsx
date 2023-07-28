3456ca868dfb687b28547f775ca683577e0222c7
Nishidh Patel <ptlmay25@gmail.com>
2023-07-28 19:20:09 +0530

    code upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ import { Geolocation } from "@capacitor/geolocation";
 const App = () => {
   const [lat, setLat] = useState("");
   const [log, setLog] = useState("");
+  const [data, setData] = useState("");
+  function fetchWeatherData(latitude, longitude) {
+    fetch(
+      `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data.address);
+      })
+      .catch((error) => console.error("Error fetching location data:", error));
+  }
 
   useEffect(() => {
     const printCurrentPosition = async () => {
@@ -11,6 +23,10 @@ const App = () => {
         const coordinates = await Geolocation.getCurrentPosition();
         setLat(coordinates.coords.latitude);
         setLog(coordinates.coords.longitude);
+        fetchWeatherData(
+          coordinates.coords.latitude,
+          coordinates.coords.longitude
+        );
         console.log(
           "Current position:",
           coordinates.coords.latitude,
@@ -28,6 +44,7 @@ const App = () => {
       <div>
         <p>Latitude:{lat} </p>
         <p>Longitude: {log} </p>
+        <p>Longitude: {data.leisure} </p>
       </div>
     </div>
   );
